8d504eb8e992aec462e58e14534499979b213d7f
Elly Wenani <ellywenani@gmail.com>
2020-05-25 19:25:28 +0300

    Commit for splash

--- app/src/main/java/com/example/christianbooklibrary/MainActivity.java
+++ app/src/main/java/com/example/christianbooklibrary/MainActivity.java
@@ -2,13 +2,60 @@ package com.example.christianbooklibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    //Variables
+    ImageView image;
+    TextView logo, dev;
+    Animation topAnim, bottomAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Hooks
+        image = findViewById(R.id.img_load);
+        logo = findViewById(R.id.txtLoad);
+        dev = findViewById(R.id.devLoad);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        dev.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent callMain = new Intent(MainActivity.this, HomeActivity.class);
+
+                Pair[] pairs = new Pair[3];
+                pairs[0] = new Pair<View, String>(image, "logo_image");
+                pairs[1] = new Pair<View, String>(logo, "logo_text");
+                pairs[2] = new Pair<View, String>(logo, "logo_devtext");
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+                    startActivity(callMain, options.toBundle());
+                    MainActivity.this.finish();
+                }
+            }
+        }, SPLASH_SCREEN);
     }
 }
