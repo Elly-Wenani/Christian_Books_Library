66389754d540674fcc6382b234eba755a7505719
Elly Wenani <ellywenani@gmail.com>
2020-05-27 01:12:42 +0300

    Added a login page with a white background

--- app/src/main/java/com/example/christianbooklibrary/HomeActivity.java
+++ app/src/main/java/com/example/christianbooklibrary/HomeActivity.java
@@ -2,13 +2,30 @@ package com.example.christianbooklibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeActivity extends AppCompatActivity {
+
+    FloatingActionButton myFlo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        myFlo = findViewById(R.id.myFloatingB);
+        myFlo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
